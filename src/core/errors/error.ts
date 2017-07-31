dd67294105e9e270bc9c46062c54bd0200632e2c
Primoz Hadalin <primoz@genialis.com>
2017-07-31 16:28:18 +0200

    Fix extending GenError

--- src/core/errors/error.ts
+++ src/core/errors/error.ts
@@ -9,5 +9,7 @@
 export class GenError extends Error {
     constructor(message: string) {
         super(message);
+        // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+        Object['setPrototypeOf'](this, GenError.prototype);
     }
 }
